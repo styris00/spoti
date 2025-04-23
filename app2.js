7c993cdde73121e58bf5ec5fc2cae4568726f1d3
styris00 <144701257+styris00@users.noreply.github.com>
2025-04-23 23:06:48 +0200

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,3 +1,10 @@
+// Service Worker pour avoir une web app native indépendante installable
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(() => console.log('Service Worker registered'))
+    .catch(error => console.log('SW registration failed:', error));
+}
+
 // ==============================================
 // Navigation entre les sections de l'application
 // ==============================================
