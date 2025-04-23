c6ea0227b8c819fda4ada7107ab47d99c9a9f22c
styris00 <144701257+styris00@users.noreply.github.com>
2025-04-23 15:04:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'v1';
-const urlsToCache = ['/', '/index.html', '/style.css', '/script.js'];
+const urlsToCache = ['/spoti/', '/spoti/index.html', '/spoti/style.css', '/spoti/script.js'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
