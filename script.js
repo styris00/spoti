419cfacb6829f9dedb06e426ac382e5579fa08ca
styris00 <144701257+styris00@users.noreply.github.com>
2025-04-23 14:52:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(() => console.log('Service Worker registered'))
+    .catch(error => console.log('SW registration failed:', error));
+}
+
+
 document.getElementById('myButton').addEventListener('click', function() {
     alert('Bouton cliqué!');
 });
