d39b66a3844b3d0a6cad89adb7b61b9b0af053e1
styris00 <144701257+styris00@users.noreply.github.com>
2025-04-23 15:01:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('sw.js')
     .then(() => console.log('Service Worker registered'))
     .catch(error => console.log('SW registration failed:', error));
 }
